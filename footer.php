c9fce7b36b5454829ab75134cfb520ed61cee0c1
xiaokang pu <pxk@seiue.com>
2018-01-09 21:08:41 +0800

    remove post nav for now

--- footer.php
+++ footer.php
@@ -3,8 +3,5 @@
 </div>
 
 <?php wp_footer(); ?>
-<?php if (is_single()): ?>
-<script type="text/javascript" src="<?php echo esc_url( get_template_directory_uri() ); ?>/post.js"></script>
-<?php endif; ?>
 </body>
 </html>
